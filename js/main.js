18d7597dc029c22078fc756347850606b74ccf4f
Julian Brodsky <brodskyjulian@yahoo.com>
2022-07-20 15:04:22 -0600

    search works better and display clears before each new search result displays

--- js/main.js
+++ js/main.js
@@ -2,20 +2,26 @@ document.querySelector('button').addEventListener('click', conductSearch)
 
 function conductSearch(){
 let userQuery = document.querySelector('input').value
+let ul = document.querySelector('.resultsUL')
 
 document.querySelector('.results').classList.remove('hide')
 
-fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${userQuery}&app_id=b76dc390&app_key=8cdd68de3e3deb4065a37433c066f003&diet=low-carb&health=vegan`)
+fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${userQuery}&app_id=b76dc390&app_key=8cdd68de3e3deb4065a37433c066f003&health=keto-friendly&health=soy-free&health=gluten-free&health=vegan`)
 .then(res => res.json())
 .then(data => {
-    console.log(data)
+    while(ul.firstChild){
+        ul.removeChild(ul.firstChild)
+    }
     data.hits.forEach( (item) => {
         console.log(item)
         let li = document.createElement('li')
         document.querySelector('.resultsUL').appendChild(li)
-        li.textContent = item.recipe.url
+        li.textContent = `${item.recipe.label} ${item.recipe.url}`
     })
     
+})
+.catch((err) => {
+    console.error(err)
 })
 
 }
