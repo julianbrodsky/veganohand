6d3d2dec24c534482fbe3f3f085e710e1aa57c50
Julian Brodsky <brodskyjulian@yahoo.com>
2022-07-20 15:13:56 -0600

    added scroll but still need to make it smooth

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ let ul = document.querySelector('.resultsUL')
 
 document.querySelector('.results').classList.remove('hide')
 
+//api request currently filters for keto friendly, soy free, gluten free, and vegan
 fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${userQuery}&app_id=b76dc390&app_key=8cdd68de3e3deb4065a37433c066f003&health=keto-friendly&health=soy-free&health=gluten-free&health=vegan`)
 .then(res => res.json())
 .then(data => {
@@ -18,6 +19,8 @@ fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${userQuery}&app_id=b
         document.querySelector('.resultsUL').appendChild(li)
         li.textContent = `${item.recipe.label} ${item.recipe.url}`
     })
+
+    ul.scrollIntoView({behavior: 'smooth', block: "center"})
     
 })
 .catch((err) => {
