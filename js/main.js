a063c541f28d26947ce9537146f4bd328cae1df7
Julian Brodsky <brodskyjulian@yahoo.com>
2022-07-21 15:44:53 -0600

    added tags and did some oop stuff with classes to make code prettier

--- js/main.js
+++ js/main.js
@@ -1,23 +1,67 @@
+//tags!
+let tags = '&health=vegan'
+
+class MakeTags{
+    constructor(selector, link){
+        this._selector = selector
+        this._link = link
+    }
+    get selector(){
+        return(this._selector)
+    }
+    get link(){
+        return(this._link)
+    }
+    clickTag(){
+        if(tags.includes(this.link)){
+            tags = tags.replace(this.link, '')
+            document.querySelector(this.selector).style.backgroundColor = '#87ba52'
+         }else{
+            tags += this.link
+            document.querySelector(this.selector).style.backgroundColor = '#85a167'
+         }
+    }
+}
+
+const gluten = new MakeTags('.gluten', '&health=gluten-free')
+document.querySelector(gluten.selector).addEventListener('click', function(){gluten.clickTag()})
+
+const keto = new MakeTags('.keto', '&health=keto-friendly')
+document.querySelector(keto.selector).addEventListener('click', function(){keto.clickTag()})
+
+const paleo = new MakeTags('.paleo', '&health=paleo')
+document.querySelector(paleo.selector).addEventListener('click', function(){paleo.clickTag()})
+
+const peanut = new MakeTags('.peanut', '&health=peanut-free')
+document.querySelector(peanut.selector).addEventListener('click', function(){peanut.clickTag()})
+
+const treenut = new MakeTags('.treenut', '&health=tree-nut-free')
+document.querySelector(treenut.selector).addEventListener('click', function(){treenut.clickTag()})
+
+const soy = new MakeTags('.soy', '&health=soy-free')
+document.querySelector(soy.selector).addEventListener('click', function(){soy.clickTag()})
+
+const kidney = new MakeTags('.kidney', '&health=kidney-friendly')
+document.querySelector(kidney.selector).addEventListener('click', function(){kidney.clickTag()})
+
+const carb = new MakeTags('.carb', '&diet=low-carb')
+document.querySelector(carb.selector).addEventListener('click', function(){carb.clickTag()})
+
+
 //event listener to search button to search upon click
 document.querySelector('.searchButton').addEventListener('click', conductSearch)
 
-//tags!
-let tags = '&health=vegan'
-document.querySelector('.gluten').addEventListener('click', gluten)
+// function gluten(){
+//     if(tags.includes('&health=gluten-free')){
+//         tags = tags.replace('&health=gluten-free', '')
+//         document.querySelector('.gluten').style.backgroundColor = '#87ba52'
+//      }else{
+//         tags += '&health=gluten-free'
+//         document.querySelector('.gluten').style.backgroundColor = '#85a167'
+//      }
+// }
 
-function gluten(){
-    if(tags.includes('&health=gluten-free')){
-        tags.replace('&health=gluten-free', '') //this line isn't working!
-        document.querySelector('.gluten').style.backgroundColor = '#87ba52'
-        console.log('includes' + tags)
-     }else{
-        tags += '&health=gluten-free'
-        document.querySelector('.gluten').style.backgroundColor = '#85a167'
-        console.log('doesnt include' + tags)
-     }
-}
-
-
+//tags constructor
 
 
 
